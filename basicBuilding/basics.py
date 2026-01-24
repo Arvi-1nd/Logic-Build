bc7b8a6b2d224f524d39773608c17b8964f55597
Arvi-1nd <davidarvind44@gmail.com>
2026-01-24 15:16:12 +0530

    finding the closest

--- basicBuilding/basics.py
+++ basicBuilding/basics.py
@@ -55,3 +55,21 @@ swapTwo = lambda a,b : (b,a)
 
 print(swapTwoVariables(3,4))
 print(swapTwo(8,9))
+
+# Finding the Closest
+
+def find_closest(n,m):
+    closest = 0
+    min_difference = float('inf')
+    
+    
+    for i in range(n - abs(m), n + abs(m) + 1):
+        if i % m == 0:
+            difference = abs(n-i)
+            
+            if difference < min_difference or  (difference == min_difference and abs(i) > abs(closest)):
+                closest = i
+                min_difference = difference
+    return closest
+
+print(find_closest(13,4))
